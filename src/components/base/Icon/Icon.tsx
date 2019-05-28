8ff32e7352891d5ad29eeb760806cf3560ce2cc3
mufdvr <belora2010@yandex.ru>
2019-05-28 15:36:13 +0300

    add icons

---
+++ src/components/base/Icon/Icon.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import classnames from 'classnames'
+
+import styles from './icon.scs'
+
+export interface IProps {
+  name: string
+  className?: string
+}
+
+const Icon: React.FC<IProps> = ({ name, className }) {
+  const
+}
+
+export default Icon
