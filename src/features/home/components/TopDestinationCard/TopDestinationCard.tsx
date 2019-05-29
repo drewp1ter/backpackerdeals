539ccb2569ac514cd9f9cf6073154c9253ce80c1
mufdvr <belora2010@yandex.ru>
2019-05-29 21:12:03 +0300

    feat(standart) added TopDestinationCard

---
+++ src/features/home/components/TopDestinationCard/TopDestinationCard.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+import styles from './TopDestinationCard.module.scss'
+
+export interface IProps {
+  readonly title: string
+  readonly country: string
+  readonly img: string
+  readonly width: 'sm' | 'md'
+  readonly height: 'sm' | 'md' | 'lg'
+  readonly gradDeg?: number
+}
+
+export const TopDestinationCard: React.FC<IProps> = ({ title, country, img, width, height, gradDeg = 0 }) => {
+  return (
+    <div className={styles.main} data-width={width} data-height={height}>
+      <div>
+        <span>{country}</span>
+        <h3>{title}</h3>
+      </div>
+      <div style={{ backgroundImage: `linear-gradient(${gradDeg}deg, rgba(232, 232, 232, 0), rgba(0, 0, 0, 0.91))` }} />
+      <img alt="" src={img} />
+    </div>
+  )
+}
---
+++ src/features/home/components/TopDestinationCard/index.ts
@@ -0,0 +1,1 @@
+export { TopDestinationCard } from './TopDestinationCard'
