be52cd802b98bb5cf98825624df63930008860e7
mufdvr <belora2010@yandex.ru>
2019-05-29 21:07:20 +0300

    refactor(Icon): convert images to base64

--- src/components/base/Icon/Icon.tsx
+++ src/components/base/Icon/Icon.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react'
 
-import svg from './svg'
+import images from './assets'
 
 const sizes = {
   md: 64
 }
 
 export interface IProps {
-  readonly name: keyof typeof svg
+  readonly name: keyof typeof images
   readonly size?: keyof typeof sizes
   readonly className?: string
 }
 
 export const Icon: React.FC<IProps> = ({ name, className, size = 'md' }) => {
-  return <img className={className} width={sizes[size]} height={sizes[size]} src={svg[name]} />
+  return <img alt="" className={className} width={sizes[size]} height={sizes[size]} src={images[name]} />
 }
--- src/features/home/components/HomeLayout/HomeLayout.tsx
+++ src/features/home/components/HomeLayout/HomeLayout.tsx
@@ -1,46 +1,99 @@
 import * as React from 'react'
 
 import { Page, Icon } from 'components/base'
-import { ActivityCard, CardsWrapper, AuthenticCard, InstagramCard } from '..'
+import { ActivityCard, CardsWrapper, AuthenticCard, InstagramCard, TravelStoryCard, TopDestinationCard } from '..'
 
-<<<<<<< HEAD
-import { ActivitiesToursThings, AuthenticReviews, InsprationInstagram, LastMinuteDeal } from '..'
+import styles from './HomeLayout.module.scss'
+import images from './assets'
 
-const HomeLayout: React.FC = () => (
-  <Page>
-    <InsprationInstagram />
-    <AuthenticReviews />
-    <ActivitiesToursThings />
-    <LastMinuteDeal />
-=======
-import styles from './homeLayout.module.scss'
-
-const HomeLayout: React.FC = () => (
+export const HomeLayout: React.FC = () => (
   <Page>
     <CardsWrapper title="Why book with us?" linkTitle="Learn more >>">
       <div className={styles.whyBook}>
-        <Icon name="liveAvailablityInstantConfirmations" />
-        <Icon name="bestSelections" />
-        <Icon name="securePayment" />
-        <Icon name="guarantee" />
-        <Icon name="liveChatExpertAgentAssistance" />
-        <Icon name="verifiedTourOperators" />
+        <div>
+          <Icon name="liveAvailablityInstantConfirmations" />
+          <span>Live Availablity</span>
+        </div>
+        <div>
+          <Icon name="bestSelections" />
+          <span>Best Selections</span>
+        </div>
+        <div>
+          <Icon name="securePayment" />
+          <span>100% Safe Payments</span>
+        </div>
+        <div>
+          <Icon name="guarantee" />
+          <span>100% Guarantee</span>
+        </div>
+        <div>
+          <Icon name="liveChatExpertAgentAssistance" />
+          <span>Live Chat</span>
+        </div>
+        <div>
+          <Icon name="verifiedTourOperators" />
+          <span>Verified Tour Operators</span>
+        </div>
+      </div>
+    </CardsWrapper>
+
+    <CardsWrapper title="Top Destinations" linkTitle="Expolore more destinations">
+      <div className={styles.cardsRow}>
+        <TopDestinationCard title="Great Barrier Reef" country="Australia" width="md" height="lg" img={images.maskGroup16} gradDeg={219} />
+        <div className={styles.topDestinationsCardsColumn}>
+          <div className={styles.cardsRow}>
+            <TopDestinationCard title="Rotorua" country="New Zealand" width="sm" height="md" img={images.maskGroup18} gradDeg={213} />
+            <TopDestinationCard title="Bangkok" country="Thailand" width="md" height="md" img={images.maskGroup20} gradDeg={234} />
+          </div>
+          <div className={styles.cardsRow}>
+            <TopDestinationCard title="Queenstown" country="New Zealand" width="md" height="sm" img={images.maskGroup19} gradDeg={245} />
+            <TopDestinationCard title="Waitomo" country="New Zealand" width="sm" height="sm" img="54" gradDeg={226} />
+          </div>
+        </div>
+        <TopDestinationCard title="Uluru" country="Australia" width="sm" height="lg" img={images.maskGroup17} gradDeg={201} />
       </div>
     </CardsWrapper>
 
     <CardsWrapper title="Activities, Tours & Things To D" linkTitle="Show more >>">
       <div key="1" className={styles.activitiesTourThingsCardsRow}>
-        <ActivityCard title="Dive & Snorkelling" img="lol" />
-        <ActivityCard title="Multi-days Tours" img="lol" />
-        <ActivityCard title="Surfing & SUP" img="lol" />
-        <ActivityCard title="Whale & dolphin watching" img="lol" />
+        <ActivityCard title="Dive & Snorkelling" variant="diveSnorkelling" />
+        <ActivityCard title="Multi-days Tours" variant="multidaysTours" />
+        <ActivityCard title="Surfing & SUP" variant="surfingSUP" />
+        <ActivityCard title="Whale & dolphin watching" variant="whaleDolphinWatching" gradDeg={209} />
       </div>
 
       <div key="2" className={styles.activitiesTourThingsCardsRow}>
-        <ActivityCard title="Classes & Workshops" img="lol" />
-        <ActivityCard title="Skydiving" img="lol" />
-        <ActivityCard title="Bungee Jumping" img="lol" />
-        <ActivityCard title="Attractions" img="lol" />{' '}
+        <ActivityCard title="Classes & Workshops" variant="classesWorkshops" />
+        <ActivityCard title="Skydiving" variant="skydiving" gradDeg={209} />
+        <ActivityCard title="Bungee Jumping" variant="bungeeJumping" />
+        <ActivityCard title="Attractions" variant="attractions" />
+      </div>
+    </CardsWrapper>
+
+    <CardsWrapper title="Travel stories" linkTitle="Read more">
+      <TravelStoryCard
+        key="1"
+        img={images.featuredArticle}
+        title="HOW TO TRAVEL BY RV ON A BACKPACKER BUDGET"
+        description="America, Backpacking Tips, Budget tips, Road Trip, RV"
+        variant="lg"
+        gradDeg={252}
+      />
+      <div key="2" className={styles.travelStoriesCardsRow}>
+        <TravelStoryCard
+          img={images.article}
+          title="10 TIPS EVERY BACKPACKER SHOULD KNOW"
+          description="America, Backpacking Tips, Budget tips"
+          variant="md"
+          gradDeg={250}
+        />
+        <TravelStoryCard
+          img={images.maskGroup15}
+          title="WHY I LOVE SOLO TRAVEL (AND WHY YOU WILL TO)"
+          description="America, Backpacking Tips, Budget tips"
+          variant="sm"
+          gradDeg={234}
+        />
       </div>
     </CardsWrapper>
 
@@ -67,7 +120,18 @@ const HomeLayout: React.FC = () => (
       </div>
     </CardsWrapper>
 
-    <CardsWrapper title="Inspiration from our instagram" linkTitle="Follow us >>">
+    <CardsWrapper title="Promoting the best of" linkTitle="Learn more">
+      <div className={styles.cardsRow}>
+        <img alt="" src={images.p1} />
+        <img alt="" src={images.p2} />
+        <img alt="" src={images.p3} />
+        <img alt="" src={images.p4} />
+        <img alt="" src={images.p5} />
+        <img alt="" src={images.p6} />
+      </div>
+    </CardsWrapper>
+
+    <CardsWrapper title="Inspiration from our instagram" linkTitle="Follow us">
       <div className={styles.cardsRow}>
         <InstagramCard img="none" />
         <InstagramCard img="none" />
@@ -77,8 +141,5 @@ const HomeLayout: React.FC = () => (
         <InstagramCard img="none" />
       </div>
     </CardsWrapper>
->>>>>>> 8bb8108dcb0ed5a434dad38c1717556946b06c86
   </Page>
 )
-
-export default HomeLayout
