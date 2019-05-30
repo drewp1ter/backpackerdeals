3cccc4d4fd8c4d71ed3b2f1101f2219b13a7e38d
mufdvr <belora2010@yandex.ru>
2019-05-30 20:19:08 +0300

    refactor(standart)

---
+++ src/components/base/Icon/assets_ld/index.ts
@@ -0,0 +1,68 @@
+import guarantee from './book/guarantee'
+import bestSelections from './book/bestSelections'
+import awardWinningTravelAgency from './book/awardWinningTravelAgency'
+import liveAvailablityInstantConfirmations from './book/liveAvailablityInstantConfirmations'
+import liveChatExpertAgentAssistance from './book/liveChatExpertAgentAssistance'
+import securePayment from './book/securePayment'
+import verifiedTourOperators from './book/verifiedTourOperators'
+import message from './book/message'
+
+import cart1 from './cart/cart1'
+import cart2 from './cart/cart2'
+import cart3 from './cart/cart3'
+import cart4 from './cart/cart4'
+
+import deal1 from './deal/deal1'
+import deal2 from './deal/deal2'
+import deal3 from './deal/deal3'
+import deal4 from './deal/deal4'
+import deal5 from './deal/deal5'
+import deal6 from './deal/deal6'
+import deal7 from './deal/deal7'
+import deal8 from './deal/deal8'
+import deal9 from './deal/deal9'
+import deal10 from './deal/deal10'
+import deal11 from './deal/deal11'
+import deal12 from './deal/deal12'
+import deal13 from './deal/deal13'
+import deal14 from './deal/deal14'
+import deal15 from './deal/deal15'
+import deal16 from './deal/deal16'
+
+import badge2 from './deal/badge2'
+import badge3 from './deal/badge3'
+import group3 from './group3.svg'
+
+export default {
+  guarantee,
+  message,
+  group3,
+  bestSelections,
+  awardWinningTravelAgency,
+  liveAvailablityInstantConfirmations,
+  liveChatExpertAgentAssistance,
+  securePayment,
+  verifiedTourOperators,
+  cart1,
+  cart2,
+  cart3,
+  cart4,
+  deal1,
+  deal2,
+  deal3,
+  deal4,
+  deal5,
+  deal6,
+  deal7,
+  deal8,
+  deal9,
+  deal10,
+  deal11,
+  deal12,
+  deal13,
+  deal14,
+  deal15,
+  deal16,
+  badge2,
+  badge3,
+}
