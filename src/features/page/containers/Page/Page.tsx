081ad122d116b3dc411269271a861c17ce357b0c
AnkarionTemios <pavel.emelyanov@webideal.pro>
2019-06-03 18:21:16 +0300

    feat(Cookies): add Cookies component

--- src/features/page/containers/Page/Page.tsx
+++ src/features/page/containers/Page/Page.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react'
 
-import { Footer, Header } from '../../components'
+import { Footer, Header, Cookies } from '../../components'
 
 interface IProps {
   readonly children?: React.ReactNode
 }
 
-export const Page: React.FC<IProps> = ({ children }) => (
-  <>
-    <Header />
-    {children}
-    <Footer />
-  </>
-)
+export const Page: React.FC<IProps> = ({ children }) => {
+  const [cookies, setCookies] = React.useState<boolean>(true)
+
+  React.useEffect(() => {
+    if (window) {
+      if (window.localStorage.getItem('cookies')) {
+        setCookies(true)
+      }
+    }
+  })
+
+  const handleCookies = () => {
+    window.localStorage.setItem('cookies', 'true')
+    setCookies(true)
+  }
+
+  return (
+    <>
+      {!cookies && <Cookies setCookies={handleCookies} />}
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
