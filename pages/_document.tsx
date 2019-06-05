592b0adec4e1ce9aadc3fe55c6cea56348b4d85e
mufdvr <belora2010@yandex.ru>
2019-06-05 22:52:58 +0300

    refactor(standart) chrome audit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,10 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>
+        <Head />
+        {/*<Head>
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossOrigin="anonymous" />
-        </Head>
+        </Head>*/}
         <body>
           <Main />
           <NextScript />
