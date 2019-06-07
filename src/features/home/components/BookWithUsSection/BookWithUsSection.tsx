43a64ad4a0423ad9a36711b470bbaf563d750f35
AnkarionTemios <pavel.emelyanov@webideal.pro>
2019-06-07 15:24:44 +0300

    feat(whyBookSection): add respove styles for whyBookSection

--- src/features/home/components/BookWithUsSection/BookWithUsSection.tsx
+++ src/features/home/components/BookWithUsSection/BookWithUsSection.tsx
@@ -9,27 +9,28 @@ export const BookWithUsSection: React.FC = () => (
   <section className={styles.bookWithUsSection}>
     <CardsWrapper title="Why book with us?" linkTitle="Learn more">
       <div className={styles.whyBook}>
-        <div>
+        <div className={styles.icon}>
           <Icon name="liveAvailablityInstantConfirmations" size="md" />
           <p>Live Availablity</p>
         </div>
-        <div>
+        <div className={styles.icon}>
           <Icon name="bestSelections" size="md" />
           <p>Best Selections</p>
         </div>
-        <div>
+        <div className={styles.icon}>
           <Icon name="securePayment" size="md" />
           <p>100% Safe Payments</p>
         </div>
-        <div>
+
+        <div className={styles.icon}>
           <Icon name="guarantee" size="md" />
           <p>100% Guarantee</p>
         </div>
-        <div>
+        <div className={styles.icon}>
           <Icon name="liveChatExpertAgentAssistance" size="md" />
           <p>Live Chat</p>
         </div>
-        <div>
+        <div className={styles.icon}>
           <Icon name="verifiedTourOperators" size="md" />
           <p>Verified Tour Operators</p>
         </div>
