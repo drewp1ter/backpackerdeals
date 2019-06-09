55759a738dc7cf7253a2b7fb663e9942e2ce1e3d
mufdvr <belora2010@yandex.ru>
2019-06-09 14:32:21 +0300

    refactor(standart)

--- src/features/home/components/ActivityCard/ActivityCard.tsx
+++ src/features/home/components/ActivityCard/ActivityCard.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import styles from './ActivityCard.module.scss'
-import * as images from './assets'
+import images from './assets'
 
 export interface IProps {
   readonly title: string
-  readonly variant: keyof typeof images.s1280
+  readonly variant: keyof typeof images
   readonly gradDeg?: number
 }
 
@@ -15,7 +15,7 @@ export const ActivityCard: React.FC<IProps> = ({ title, variant, gradDeg = 330 }
       <h3>{title}</h3>
       <span>24 Activities</span>
       <div style={{ backgroundImage: `linear-gradient(${gradDeg}deg, rgba(232, 232, 232, 0), rgba(0, 0, 0, 0.91))` }} />
-      <img srcSet={`${images.s1280[variant]} 1280w, ${images.s1920[variant]} 1920w`} src={images.s1920[variant]} alt="Activity" />
+      <img src={images[variant]} alt="Activity" />
     </div>
   )
 }
--- src/features/home/components/ActivityCard/assets/index.ts
+++ src/features/home/components/ActivityCard/assets/index.ts
@@ -1,38 +1,10 @@
-import './diveSnorkelling.jpg'
-import './multidaysTours.jpg'
-import './surfingSUP.jpg'
-import './whaleDolphinWatching.jpg'
-import './classesWorkshops.jpg'
-import './skydiving.jpg'
-import './bungeeJumping.jpg'
-import './attractions.jpg'
-import './diveSnorkelling256.jpg'
-import './multidaysTours256.jpg'
-import './surfingSUP256.jpg'
-import './whaleDolphinWatching256.jpg'
-import './classesWorkshops256.jpg'
-import './skydiving256.jpg'
-import './bungeeJumping256.jpg'
-import './attractions256.jpg'
-
-export const s1920 = {
-  diveSnorkelling: require('./diveSnorkelling.jpg?webp'),
-  multidaysTours: require('./multidaysTours.jpg?webp'),
-  surfingSUP: require('./surfingSUP.jpg?webp'),
-  whaleDolphinWatching: require('./whaleDolphinWatching.jpg?webp'),
-  classesWorkshops: require('./classesWorkshops.jpg?webp'),
-  skydiving: require('./skydiving.jpg?webp'),
-  bungeeJumping: require('./bungeeJumping.jpg?webp'),
-  attractions: require('./attractions.jpg?webp'),
-}
-
-export const s1280 = {
-  diveSnorkelling: require('./diveSnorkelling256.jpg?webp'),
-  multidaysTours: require('./multidaysTours256.jpg?webp'),
-  surfingSUP: require('./surfingSUP256.jpg?webp'),
-  whaleDolphinWatching: require('./whaleDolphinWatching256.jpg?webp'),
-  classesWorkshops: require('./classesWorkshops256.jpg?webp'),
-  skydiving: require('./skydiving256.jpg?webp'),
-  bungeeJumping: require('./bungeeJumping256.jpg?webp'),
-  attractions: require('./attractions256.jpg?webp'),
+export default {
+  diveSnorkelling: require('./diveSnorkelling.jpg'),
+  multidaysTours: require('./multidaysTours.jpg'),
+  surfingSUP: require('./surfingSUP.jpg'),
+  whaleDolphinWatching: require('./whaleDolphinWatching.jpg'),
+  classesWorkshops: require('./classesWorkshops.jpg'),
+  skydiving: require('./skydiving.jpg'),
+  bungeeJumping: require('./bungeeJumping.jpg'),
+  attractions: require('./attractions.jpg'),
 }
