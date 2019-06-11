928555f511fe006944fda11a2f336cf55da439da
mufdvr <belora2010@yandex.ru>
2019-06-11 21:55:31 +0300

    feat(TopDestinations) add response

---
+++ src/features/home/components/TravelStoriesSection/TravelStoriesSection.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+import { CardsWrapper, TravelStoryCard } from '..'
+import styles from './TravelStoriesSection.module.scss'
+
+export const TravelStoriesSection: React.FC = () => (
+  <CardsWrapper title="Travel stories" linkTitle="Read more">
+    <div className={styles.travelStories}>
+      <TravelStoryCard
+        title="HOW TO TRAVEL BY RV ON A BACKPACKER BUDGET"
+        description="America, Backpacking Tips, Budget tips, Road Trip, RV"
+        variant="image63"
+        gradDeg={252}
+        className={styles.item1}
+      />
+      <TravelStoryCard
+        title="10 TIPS EVERY BACKPACKER SHOULD KNOW"
+        description="America, Backpacking Tips, Budget tips"
+        variant="image64"
+        gradDeg={250}
+        className={styles.item2}
+      />
+      <TravelStoryCard
+        title="WHY I LOVE SOLO TRAVEL (AND WHY YOU WILL TO)"
+        description="America, Backpacking Tips, Budget tips"
+        variant="image65"
+        gradDeg={234}
+        className={styles.item3}
+      />
+    </div>
+  </CardsWrapper>
+)
