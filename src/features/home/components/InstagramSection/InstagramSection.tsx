25e84fdf10da0094f2c7b00b177350289be224a5
mufdvr <belora2010@yandex.ru>
2019-06-13 22:32:36 +0300

    fix(InstagrammSection) infinity scroll

--- src/features/home/components/InstagramSection/InstagramSection.tsx
+++ src/features/home/components/InstagramSection/InstagramSection.tsx
@@ -10,12 +10,10 @@ export interface IProps {
 
 export const InstagramSection: React.FC<IProps> = ({ className }) => {
   const setStartPos = (target: any) => {
-    const startOffset = 1374
     if (!target) {
       return
     }
-    const { offsetWidth } = target
-    target.scrollTo(startOffset - offsetWidth, 0)
+    target.scrollTo(810, 0)
   }
 
   const renderCard = (img: any, key?: string, forCarousel: boolean = false) => (
@@ -29,14 +27,15 @@ export const InstagramSection: React.FC<IProps> = ({ className }) => {
     </div>
   )
 
-  const renderCards = (forCarousel: boolean = false) => Object.entries(images).map((img: [string, any]) => renderCard(img[1], img[0], forCarousel))
+  const renderCards = (forCarousel: boolean = false) =>
+    Object.entries(images).map((img: [string, any]) => renderCard(img[1], img[0], forCarousel))
 
   const handleScroll = ({ target }: any) => {
     const { scrollLeft, offsetWidth } = target
-    const maxScrollRight = 1555
-    const maxScrollLeft = 732
+    const maxScrollRight = 1680
+    const maxScrollLeft = 514
     const offsetRight = 812
-    const offsetLeft = 1540
+    const offsetLeft = 1325
     if (scrollLeft + offsetWidth >= maxScrollRight) {
       target.scrollTo(maxScrollRight - offsetWidth - offsetRight, 0)
     }
@@ -49,7 +48,8 @@ export const InstagramSection: React.FC<IProps> = ({ className }) => {
     <CardsWrapper className={className} title="Inspiration from our instagram" linkTitle="Follow us">
       <div ref={setStartPos} onScroll={handleScroll} className={styles.instagramCards}>
         {renderCards()}
-        {renderCards(true)}
+        {renderCards()}
+        {renderCard(images.v1, 'card', true)}
       </div>
     </CardsWrapper>
   )
