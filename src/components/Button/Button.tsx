c50e358ebc806c2e665b15fc05ea50e53dfac6d3
mufdvr <belora2010@yandex.ru>
2019-06-20 05:26:27 +0300

    feat(Login)

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+
+import classNames from 'classnames'
+
+import styles from './Button.module.scss'
+
+export interface IProps {
+  readonly children?: React.ReactNode
+  readonly className?: string
+  readonly disabled?: boolean
+  readonly onClick?: () => void
+  readonly form?: 'rectangled' | 'rounded' | 'circle'
+  readonly size?: 'md'
+  readonly theme?: 'orange' | 'transparentBorderWhite' | 'transparentBorderOrange'
+}
+
+export const Button: React.FC<IProps> = ({
+  className = '',
+  children,
+  onClick,
+  disabled = false,
+  form = 'rounded',
+  theme = 'orange',
+  size = 'md'
+}) => {
+  return (
+    <button
+      aria-label="button"
+      className={classNames(styles.button, className)}
+      disabled={disabled}
+      onClick={onClick}
+      data-form={form}
+      data-theme={theme}
+      data-size={size}
+    >
+      {children}
+    </button>
+  )
+}
