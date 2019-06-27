ab5c253a1e67f654d4c4bdbc3d1d8bf1236491bb
mufdvr <belora2010@yandex.ru>
2019-06-27 19:23:11 +0300

    feat(MyProfile)

--- pages/account/my_profile.tsx
+++ pages/account/my_profile.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 import { MyProfile } from 'features/account/containers'
 import { Page } from 'features/page/components'
+import { ProfileLayout } from 'features/account/components'
 
 class Catalog extends React.Component {
   render = () => (
     <Page headerTheme="dark">
-      <MyProfile />
+      <ProfileLayout>
+        <MyProfile />
+      </ProfileLayout>
     </Page>
   )
 }
--- src/features/account/containers/MyProfile/MyProfile.tsx
+++ src/features/account/containers/MyProfile/MyProfile.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
 
-import { Input, Button } from 'components'
+import { Autocomplete, Button, Input } from 'components'
 
 import styles from './MyProfile.module.scss'
 
 export const MyProfile: React.FC = () => {
-
   return (
     <div className={styles.myProfile}>
-      <h2>My profile</h2>
+      <h3>My profile</h3>
       <div className={styles.photo}>
-        <div className={styles.image}>
-          JD
-        </div>
+        <div className={styles.image}>JD</div>
         <i className="fas fa-cloud-upload-alt" />
-        <label htmlFor="photo-upload">Upload photo</label>
-        <input type="file" id="photo-upload" hidden={true} />
+        <Input type="file" label="Upload photo" />
       </div>
       <div className={styles.fields}>
-        <Input theme="standart" label="Name" size="md" placeholder="Enter your first and last name" />
-        <Input theme="standart" label="Address line" size="md" placeholder="Enter your address" />
-        <Input theme="standart" label="Address line 2" size="md" placeholder="Enter your address" />
-        <Input theme="standart" label="City" size="md" placeholder="Enter your city" />
-        <Input theme="standart" label="State" size="md" placeholder="Enter your State" />
-        <Input theme="standart" label="Postcode" size="md" placeholder="Postcode" />
+        <Input theme="standart" label="Name" placeholder="Enter your first and last name" />
+        <Input theme="standart" label="Address line" placeholder="Enter your address" />
+        <Input theme="standart" label="Address line 2" placeholder="Enter your address" />
+        <Input theme="standart" label="City" placeholder="Enter your city" />
+        <Input theme="standart" label="State" placeholder="Enter your State" />
+        <Input theme="standart" label="Postcode" placeholder="Postcode" />
+        <Autocomplete
+          className={styles.autocomplete}
+          suggestions={['USA', 'Australia', 'Argentina', 'Malasya']}
+          placeholder="Type or choose your country"
+          label="Country"
+        />
+        <Input className={styles.field} theme="standart" type="email" label="Email" placeholder="Email Address" />
+        <Button>Update changes</Button>
       </div>
     </div>
   )
