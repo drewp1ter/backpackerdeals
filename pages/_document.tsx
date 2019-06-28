7fef7960dbf9a91347a59d7cdc287456eaf70c42
mufdvr <belora2010@yandex.ru>
2019-06-28 21:23:24 +0300

    feat(inline css)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { InlineStylesHead } from 'components/InlineStylesHead'
-import Document, { Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class CustomDocument extends Document {
   static getInitialProps({ renderPage }: any) {
@@ -8,12 +8,13 @@ export default class CustomDocument extends Document {
   }
 
   render() {
+    const HeadVar = process.env.NODE_ENV === 'production' ? InlineStylesHead : Head
     return (
       <html lang="en">
-        <InlineStylesHead>
+        <HeadVar>
           <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
           {/*<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossOrigin="anonymous" />*/}
-        </InlineStylesHead>
+        </HeadVar>
         <body>
           <Main />
           <NextScript />
