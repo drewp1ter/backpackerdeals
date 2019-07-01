5880aa4b8613637df3e63ef6ebcbbb629040dc9e
mufdvr <belora2010@yandex.ru>
2019-07-01 14:59:05 +0300

    refactor(TravelStoryCard): update responsive

--- src/features/home/components/TravelStoryCard/TravelStoryCard.tsx
+++ src/features/home/components/TravelStoryCard/TravelStoryCard.tsx
@@ -23,7 +23,7 @@ export const TravelStoryCard: React.FC<IProps> = ({ title, description, variant,
       </div>
       <div className={styles.gradient} style={gradient} />
       <picture>
-        <source media="(max-width: 767px)" srcSet={images.w320[variant]} />
+        <source media="(max-width: 440px)" srcSet={images.w320[variant]} />
         <source media="(max-width: 1024px)" srcSet={images.w1024[variant]} />
         <source media="(max-width: 1280px)" srcSet={images.w1280[variant]} />
         <source media="(max-width: 1920px)" srcSet={images.w1920[variant]} />
