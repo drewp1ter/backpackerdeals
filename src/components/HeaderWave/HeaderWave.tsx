f20c0e82611708560bbc00b0d543dfb711a947f6
Kirill <kirill.romanov@webideal.pro>
2019-07-02 16:30:47 +0400

    fix(Catalog): fix catalog device styles

--- src/components/HeaderWave/HeaderWave.tsx
+++ src/components/HeaderWave/HeaderWave.tsx
@@ -6,15 +6,16 @@ import styles from './HeaderWave.module.scss'
 export interface IProps {
   readonly children: JSX.Element | JSX.Element[]
   readonly className: string
-  readonly maskClassName?: string
+  readonly maskClassName?: string,
+  readonly hasGradient?: boolean
 }
 
-export const HeaderWave: React.FC<IProps> = ({ children, className, maskClassName }) => (
+export const HeaderWave: React.FC<IProps> = ({ children, className, maskClassName, hasGradient = true }) => (
   <section className={classNames(styles.headWave, className)}>
     <div className={styles.children}>
       {children}
     </div>
     <div className={classNames(styles.mask, maskClassName)} />
-    <div className={styles.gradient} />
+    {hasGradient && <div className={styles.gradient} />}
   </section>
 )
