0a955c7169f508c73d6b2814c9595036a9b362eb
Kirill <kirill.romanov@webideal.pro>
2019-07-03 00:48:05 +0400

    fix(HeaderCard): fix header card styles

--- src/features/catalog/components/ActivitiesSection/ActivitiesSection.tsx
+++ src/features/catalog/components/ActivitiesSection/ActivitiesSection.tsx
@@ -13,15 +13,19 @@ export const ActivitiesSection: React.FC = () => (
       in Australia
     </h1>
 
-    <HeaderCard
-    title="Australia"
-    description="Tours, day trips and activities in Australia"
-    body="With year round sunshine, incredible beaches and some of the most cosmopolitan cities in the world,
-    what’s not to love? Whether you want to dive the Great Barrier Reef, surf on Bondi Beach, sleep under the stars
-    at Uluru or party the night away in Melbourne, we’ve got your Australian adventure covered! It’s time to head Down Under.
-    Check out our Australia deals"
-    rating={4.5}
-    className={styles.card}
-    />
+    <div className={styles.headerCardWrapper}>
+      <aside>
+        <HeaderCard
+          title="Australia"
+          description="Tours, day trips and activities in Australia"
+          body="With year round sunshine, incredible beaches and some of the most cosmopolitan cities in the world,
+        what’s not to love? Whether you want to dive the Great Barrier Reef, surf on Bondi Beach, sleep under the stars
+        at Uluru or party the night away in Melbourne, we’ve got your Australian adventure covered! It’s time to head Down Under.
+        Check out our Australia deals"
+          rating={4.5}
+          className={styles.card}
+        />
+      </aside>
+    </div>
   </HeaderWave2>
 )
