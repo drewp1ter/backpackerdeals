6b959a0389135afb05eb42af654a39e14f2f5daa
Kirill <kirill.romanov@webideal.pro>
2019-07-04 23:43:09 +0400

    fix(Cookie): set cookies

--- src/features/page/components/Cookies/Cookies.tsx
+++ src/features/page/components/Cookies/Cookies.tsx
@@ -1,22 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import styles from './Cookies.module.scss'
 
-interface IProps {
-  readonly setCookies: () => void
-}
+export const Cookies: React.FC = () => {
+  const [hasCookies, setCookies] = React.useState<boolean>(true)
 
-export const Cookies: React.FC<IProps> = ({ setCookies }) => (
-  <div className={styles.cookiesStyle}>
-    <span>
-      Backpacker Deals processes information about your visit using cookies to improve site performance, facilitate social media sharing
-      and offer advertising tailored to your interests.
-      <br/>
-      By continuing to browse our site, you agree to the use of these cookies. For more
-      information see our Terms & Conditions and Privacy Policy.{' '}
-    </span>
-    <button
-    onClick={setCookies}
-    >OK</button>
-  </div>
-)
+  useEffect(() => {
+    const hasCookie = getCookie('disallow_page_cookies')
+
+    if (!hasCookie) {
+      setCookies(false)
+    }
+  })
+
+  const getCookie = (name: string) => {
+    const regex = new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
+    const matches = document.cookie.match(regex)
+    return matches ? decodeURIComponent(matches[1]) : undefined
+  }
+
+  const setCookie = (name: string, value: string, options?: { [key: string]: any }) => {
+    const _options = options || {}
+    const expires = _options.expires
+
+    if (expires && expires.toUTCString) {
+      _options.expires = expires.toUTCString()
+    }
+
+    let updatedCookie = name + '=' + encodeURIComponent(value)
+
+    for (const key in _options) {
+      if (_options.hasOwnProperty(key)) {
+        const optionValue = _options[key]
+        if (optionValue) {
+          updatedCookie += `; ${key}=${optionValue}`
+        }
+      }
+    }
+
+    document.cookie = updatedCookie
+  }
+
+  const handleClick = () => {
+    setCookie('disallow_page_cookies', 'true')
+    setCookies(true)
+  }
+
+  const cookieStyles = {
+    display: hasCookies ? 'none' : 'block',
+  }
+
+  return (
+    <div style={cookieStyles} className={styles.cookiesStyle}>
+      <div>
+        <span>
+          Backpacker Deals processes information about your visit using cookies to improve site performance, facilitate social media sharing
+          and offer advertising tailored to your interests.
+          <br/>
+          By continuing to browse our site, you agree to the use of these cookies. For more
+          information see our Terms & Conditions and Privacy Policy.{' '}
+        </span>
+        <button onClick={handleClick}>OK</button>
+      </div>
+    </div>
+  )
+}
--- src/features/page/components/Page/Page.tsx
+++ src/features/page/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { Cookies, Footer, Header } from '..'
 import LazyLoad from 'react-lazyload'
+import { Cookies, Footer, Header } from '..'
 
 interface IProps {
   readonly children?: React.ReactNode
@@ -8,32 +8,15 @@ interface IProps {
   readonly withoutFooter?: boolean
 }
 
-export const Page: React.FC<IProps> = ({ children, headerTheme, withoutFooter = false }) => {
-  const [cookies, setCookies] = React.useState<boolean>(true)
-
-  React.useEffect(() => {
-    if (window) {
-      if (window.localStorage.getItem('cookies')) {
-        setCookies(true)
-      }
-    }
-  })
-
-  const handleCookies = () => {
-    window.localStorage.setItem('cookies', 'true')
-    setCookies(true)
-  }
-
-  return (
-    <>
-      {!cookies && <Cookies setCookies={handleCookies} />}
-      <Header theme={headerTheme} />
-      {children}
-      {!withoutFooter && (
-        <LazyLoad height={500} offset={300}>
-          <Footer />
-        </LazyLoad>
-      )}
-    </>
-  )
-}
+export const Page: React.FC<IProps> = ({ children, headerTheme, withoutFooter = false }) => (
+  <>
+    <Cookies/>
+    <Header theme={headerTheme}/>
+    {children}
+    {!withoutFooter && (
+      <LazyLoad height={500} offset={300}>
+        <Footer/>
+      </LazyLoad>
+    )}
+  </>
+)
