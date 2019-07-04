eef44e22c963af6e7d03b9a04fc66a3c9c30d773
mufdvr <belora2010@yandex.ru>
2019-07-04 20:41:24 +0300

    feat(MyProfile)

--- src/components/PhoneNumber/PhoneNumber.tsx
+++ src/components/PhoneNumber/PhoneNumber.tsx
@@ -3,50 +3,69 @@ import React, { useState } from 'react'
 import classNames from 'classnames'
 import countries from './countries'
 
+import { ScrollBar } from '../ScrollBar'
+
 import styles from './PhoneNumber.module.scss'
 
-interface IProps {
-  readonly options?: JSX.Element[]
+export interface IProps {
   readonly className?: string
-  readonly value?: JSX.Element
-  readonly disabled?: boolean
-  readonly onChange?: () => void
+  readonly name?: string
+  readonly onChange?: (value: string, name?: string) => void
   readonly size?: 'md' | 'lg'
 }
 
-export const PhoneNumber: React.FC<IProps> = ({ children, className, options, value, disabled = false, onChange, size = 'md' }) => {
-  const [isOpen, setOpen] = useState<boolean>(false)
+interface IState {
+  readonly isOpen: boolean
+  readonly countryCodeIdx: number
+  readonly phone: string
+}
 
-  const handleClose = () => setOpen(false)
-  const toggleSelect = () => !disabled && setOpen(!isOpen)
-  const handleSelect = (event: React.MouseEvent<HTMLElement>) => {}
+export const PhoneNumber: React.FC<IProps> = ({ className, onChange, name, size = 'md' }) => {
+  const [state, setState] = useState<IState>({
+    isOpen: false,
+    countryCodeIdx: 0,
+    phone: '',
+  })
+
+  const handleClose = () => setState({ ...state, isOpen: false })
+  const toggleSelect = () => setState({ ...state, isOpen: !state.isOpen })
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const match = target.value.match(/\d+/g)
+    const phone = match ? match[0] : ''
+    setState({ ...state, phone })
+    onChange && onChange(countries[state.countryCodeIdx][1] + phone, name)
+  }
+  const handleSelect = ({ currentTarget }: React.MouseEvent<HTMLElement>) => {
+    const idx = currentTarget.dataset.idx || 0
+    setState({ ...state, countryCodeIdx: +idx, isOpen: false })
+    onChange && onChange(countries[state.countryCodeIdx][1] + state.phone, name)
+  }
 
   return (
     <div className={classNames(styles.phoneNumber, className)}>
-      <div className={styles.countryCode} tabIndex={0} onBlur={handleClose}>
-        <div
-          className={classNames(styles.currentValue, styles.option)}
-          data-opened={isOpen}
-          data-disabled={disabled}
-          data-size={size}
-          onClick={toggleSelect}
-        >
-          {countries[0][0]}
-          <span>{countries[0][1]}</span>
-          <i className="fas fa-angle-down" />
-        </div>
+      <label htmlFor="phone_number">Phone Number (If you prefer Whatsapp please enter that number)</label>
+      <div className={styles.body}>
+        <div className={styles.countryCode} tabIndex={0} onBlur={handleClose}>
+          <div className={styles.currentValue} data-opened={state.isOpen} data-size={size} onClick={toggleSelect}>
+            <div>
+              {countries[state.countryCodeIdx][0]}
+              <span>{countries[state.countryCodeIdx][1]}</span>
+            </div>
+            <i className="fas fa-angle-down" />
+          </div>
 
-        {isOpen && (
-          <ul className={styles.optionBlock} data-size={size}>
-            {countries.map((country, idx) => (
-              <li key={idx} className={styles.option} onClick={handleSelect} data-idx={idx}>
-                {country[0]}
-                <span>{country[1]}</span>
-              </li>
-            ))}
-            {children}
+          <ul data-size={size} data-opened={state.isOpen}>
+            <ScrollBar>
+              {countries.map((country, idx) => (
+                <li key={idx} className={styles.option} onClick={handleSelect} data-idx={idx}>
+                  {country[0]}
+                  <span>{country[1]}</span>
+                </li>
+              ))}
+            </ScrollBar>
           </ul>
-        )}
+        </div>
+        <input value={state.phone} onChange={handleChange} id="phone_number" type="text" placeholder="Select country code" />
       </div>
     </div>
   )
