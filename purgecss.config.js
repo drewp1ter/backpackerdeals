833bac1ee2db0515d05ba374fadfb44b04917376
mufdvr <belora2010@yandex.ru>
2019-07-05 13:40:55 +0300

    fix(Caledar): add whitelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   content: ['.next/static/**/*.js'],
-  css: ['.next/static/chunks/*.css']
+  css: ['.next/static/chunks/*.css'],
+  whitelist: [
+    'react-calendar__navigation__next-button',
+    'react-calendar__navigation__prev-button',
+    'react-calendar__navigation__next2-button',
+    'react-calendar__navigation__prev2-button',
+  ],
 }
