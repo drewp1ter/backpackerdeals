98bc2709806083a65ba5a070b45a70834c5bf5e7
Kirill <kirill.romanov@webideal.pro>
2019-07-09 21:12:11 +0400

    feat(Catalog header): fix catalog header card

--- src/features/catalog/components/ActivitiesSection/ActivitiesSection.tsx
+++ src/features/catalog/components/ActivitiesSection/ActivitiesSection.tsx
@@ -8,18 +8,16 @@ import styles from './ActivitiesSection.module.scss'
 export const ActivitiesSection: React.FC = () => (
   <HeaderWave2 className={styles.activitiesSection}>
     <div className={styles.headerCardWrapper}>
-      <aside>
-        <HeaderCard
-          title="Australia"
-          description="Tours, day trips and activities in Australia"
-          body="With year round sunshine, incredible beaches and some of the most cosmopolitan cities in the world,
+      <HeaderCard
+        title="Australia"
+        description="Tours, day trips and activities in Australia"
+        body="With year round sunshine, incredible beaches and some of the most cosmopolitan cities in the world,
         what’s not to love? Whether you want to dive the Great Barrier Reef, surf on Bondi Beach, sleep under the stars
         at Uluru or party the night away in Melbourne, we’ve got your Australian adventure covered! It’s time to head Down Under.
         Check out our Australia deals"
-          rating={4.5}
-          className={styles.card}
-        />
-      </aside>
+        rating={4.5}
+        className={styles.card}
+      />
     </div>
   </HeaderWave2>
 )
