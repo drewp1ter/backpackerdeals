ba87f922bbea5aaf4d8177b205a4bc1c6d995a3f
Kirill <kirill.romanov@webideal.pro>
2019-07-11 07:35:31 +0400

    fix(Calendar): select year, month

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,6 +1,6 @@
+import moment from 'moment'
 import React, { useState } from 'react'
 import Datepicker from 'react-calendar/dist/entry.nostyle'
-
 import './Calendar.scss'
 
 interface IProps {
@@ -8,20 +8,37 @@ interface IProps {
   onChange?: (value: Date) => void
 }
 
-export const Calendar: React.FC<IProps> = ({date, onChange}) => {
+export const Calendar: React.FC<IProps> = ({ date, onChange }) => {
   const [selectedDate, setSelectedDate] = useState<Date | Date[]>(new Date())
 
-  const handleDateChange = (date: Date | Date[]) => {
-    setSelectedDate(date)
-    onChange && onChange(date as Date)
+  const emitOnChange = (newDate: Date) => {
+    setSelectedDate(newDate)
+    onChange && onChange(newDate)
+  }
+
+  const handleDateChange = (newDate: Date | Date[]) => {
+    emitOnChange(newDate as Date)
+  }
+
+  const handleYearChange = (newDate: Date | Date[]) => {
+    const parsedNewDate = moment(date as Date).year(moment(newDate as Date).year()).toDate()
+    emitOnChange(parsedNewDate)
+  }
+
+  const handleMonthChange = (newDate: Date | Date[]) => {
+    const parsedNewDate = moment(date as Date).month(moment(newDate as Date).month()).toDate()
+    emitOnChange(parsedNewDate)
   }
 
   return (
     <Datepicker
       value={date || selectedDate}
       locale="en"
+      minDetail="decade"
       showNeighboringMonth={false}
       onChange={handleDateChange}
+      onClickYear={handleYearChange}
+      onClickMonth={handleMonthChange}
     />
   )
 }
