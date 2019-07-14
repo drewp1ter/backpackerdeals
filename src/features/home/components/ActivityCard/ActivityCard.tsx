1ccf254c8caee68b02ecaee180c510ee3d88a328
mufdvr <belora2010@yandex.ru>
2019-07-14 19:36:55 +0300

    refactor(HomePage)

--- src/features/home/components/ActivityCard/ActivityCard.tsx
+++ src/features/home/components/ActivityCard/ActivityCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 import styles from './ActivityCard.module.scss'
@@ -13,16 +14,20 @@ export const ActivityCard: React.FC<IProps> = ({ title, variant, gradDeg = 330 }
   <li className={styles.activityCard}>
     <h3>{title}</h3>
     <span>24 Activities</span>
-    <a href="#">
-      <div className={styles.gradient}
-           style={{ backgroundImage: `linear-gradient(${gradDeg}deg, rgba(232, 232, 232, 0), rgba(0, 0, 0, 0.91))` }}/>
-      <picture>
-        <source media="(max-width: 320px)" srcSet={images.w320[variant].src}/>
-        <source media="(max-width: 1024px)" srcSet={images.w1024[variant].src}/>
-        <source media="(max-width: 1280px)" srcSet={images.w1280[variant].src}/>
-        <source media="(max-width: 1920px)" srcSet={images.w1920[variant]}/>
-        <img src={images.w1920[variant]} alt="Activity"/>
-      </picture>
-    </a>
+    <Link href="#">
+      <a>
+        <div
+          className={styles.gradient}
+          style={{ backgroundImage: `linear-gradient(${gradDeg}deg, rgba(232, 232, 232, 0), rgba(0, 0, 0, 0.91))` }}
+        />
+        <picture>
+          <source media="(max-width: 320px)" srcSet={images.w320[variant].src} />
+          <source media="(max-width: 1024px)" srcSet={images.w1024[variant].src} />
+          <source media="(max-width: 1280px)" srcSet={images.w1280[variant].src} />
+          <source media="(max-width: 1920px)" srcSet={images.w1920[variant]} />
+          <img src={images.w1920[variant]} alt="Activity" />
+        </picture>
+      </a>
+    </Link>
   </li>
 )
