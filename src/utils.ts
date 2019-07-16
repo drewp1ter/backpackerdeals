f1e54b4950e400641fc2af8209d41994ae2df8c0
mufdvr <belora2010@yandex.ru>
2019-07-16 21:37:31 +0300

    feat(utils): add coments to circleScroll

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,10 @@ export interface ICircleScrollProps {
 }
 
 export const circleScroll = ({ target, maxScrollRight, maxScrollLeft, offsetRight, offsetLeft }: ICircleScrollProps) => {
+  // maxScrollRight = коордиата (scrollLeft + offsetWidth) с которой перескакивать
+  // offsetRight = прокрутить на нужное метсо и maxScrollRight - offsetWidth - scrollLeft
+  // maxScrollLeft = координата scrollLeft с которой перескакивать
+  // offsetLeft = координата scrollLeft куда перескачить
   const { scrollLeft, offsetWidth } = target
   if (scrollLeft + offsetWidth >= maxScrollRight) {
     target.scrollTo(maxScrollRight - offsetWidth - offsetRight, 0)
