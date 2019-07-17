a0655180ca2035d8a1d8922ffde06ab5a09392c7
mufdvr <belora2010@yandex.ru>
2019-07-17 13:06:15 +0300

    fix(MobileMenu): add close by select continent

--- src/features/page/containers/MobileMenu/MobileMenu.tsx
+++ src/features/page/containers/MobileMenu/MobileMenu.tsx
@@ -18,17 +18,16 @@ interface IProps {
   readonly theme?: string
 }
 
-export const MobileMenu: React.FC<Partial<Types.RootState> & PageActions & SearchActions & IProps> =
-  ({
-    search,
-    page,
-    theme,
-    openMenu,
-    closeMenu,
-    openSearch,
-    closeSearch,
-    changeSearchType,
-  }) => {
+export const MobileMenu: React.FC<Partial<Types.RootState> & PageActions & SearchActions & IProps> = ({
+  search,
+  page,
+  theme,
+  openMenu,
+  closeMenu,
+  openSearch,
+  closeSearch,
+  changeSearchType,
+}) => {
   const fields = {
     startLocation: 'Start location',
     typeOfTour: 'Type of tour',
@@ -37,26 +36,26 @@ export const MobileMenu: React.FC<Partial<Types.RootState> & PageActions & Searc
     numberOfDays: 'Number of days',
     currency: currencies.aud,
     language: languages.english,
-  };
+  }
 
   const [searchQuery, changeSearch] = React.useState('')
   const [multiDayTour, selectTourType] = React.useState(false)
-  const [selected, setSelected] = useState({...fields})
+  const [selected, setSelected] = useState({ ...fields })
 
   const handleChange = (value: any, key: string) => setSelected({ ...selected, [key]: value })
 
   // const handleChangeDate = (value: Date) => handleChange(value, 'date')
 
-  const handleClickReset = () => setSelected({...fields})
+  const handleClickReset = () => setSelected({ ...fields })
   const handleClickOption = (value: any, key: string) => {
-    blurActiveElement();
+    blurActiveElement()
     handleChange(value, key)
   }
 
   const blurActiveElement = () => {
-      if (window.document.activeElement !== null) {
-        (window.document.activeElement as HTMLElement).blur()
-      }
+    if (window.document.activeElement !== null) {
+      ;(window.document.activeElement as HTMLElement).blur()
+    }
   }
 
   const currencyFormatFn = (currency: IIconInfo) => `${currency.icon} ${currency.name}`
@@ -66,178 +65,191 @@ export const MobileMenu: React.FC<Partial<Types.RootState> & PageActions & Searc
     <div className={styles.mobileMenu} data-theme={theme}>
       <button onClick={openSearch} className={styles.searchIcon}>
         <span>Search</span>
-        <i className="fas fa-search"/>
+        <i className="fas fa-search" />
       </button>
       <button onClick={openMenu} className={styles.menuIcon}>
-        <i className="fas fa-bars"/>
+        <i className="fas fa-bars" />
       </button>
 
       <MobileMenuWrapper className={styles.menu} open={page!.menuIsOpen}>
-        <div className={styles.mobileHeader}>
-          <p>
-            <i className="fas fa-phone-alt"/>
-            <span>+ 61 3 90163720</span>
-          </p>
+        <>
+          <div className={styles.mobileHeader}>
+            <p>
+              <i className="fas fa-phone-alt" />
+              <span>+ 61 3 90163720</span>
+            </p>
 
-          <button onClick={closeMenu}>
-            <i className="fas fa-times"/>
-          </button>
-        </div>
+            <button onClick={closeMenu}>
+              <i className="fas fa-times" />
+            </button>
+          </div>
 
-        <div className={styles.navigation}>
-          <div onClick={openSearch} className={styles.menuItem}>
-            <div className={styles.title}>
-              <i className="fas fa-search"/>
-              <span>Search</span>
-            </div>
-            <div className={styles.additionalTitle}>
-              <span>{search!.searchType === 'advanced' ? 'Advanced search' : 'Basic search'}</span>
-              <i className="fas fa-filter"/>
-            </div>
-          </div>
-          <MobileSelectMenu
-            title="Explore by country"
-            leftIcon="fas fa-map-marker-alt"
-            rightIcon="fas fa-chevron-right"
-            reversableIcon="right"
-            reverseType="90"
-          >
-            <SelectContinent theme="mobile"/>
-          </MobileSelectMenu>
-          <div className={styles.menuItem}>
-            <div className={styles.title}>
-              <i className="fas fa-thumbs-up"/>
-              <span>Top Deals</span>
-            </div>
-          </div>
-          <div className={styles.menuItem}>
-            <div className={styles.title}>
-              <i className="fas fa-clock"/>
-              <span>Last Minute Deals</span>
-            </div>
-          </div>
-          <div className={styles.menuItem}>
-            <div className={styles.title}>
-              <i className="fas fa-user-alt"/>
-              <span>Sign up</span>
-            </div>
-          </div>
-          <h5>Settings</h5>
-          <MobileSelectMenu
-            childrenClassName={styles.currencies}
-            leftIcon="fas fa-chevron-down"
-            reversableIcon="left"
-            selected={selected.currency}
-            format={currencyFormatFn}
-            reverseType="180"
-          >
-            {Object.entries(currencies).map((currency, index) => (
-              <p key={`${currency[0]}-${index}`}
-                 data-currency={currency[1]}
-                 data-field="currency"
-                 onClick={() => handleClickOption(currency[1], 'currency')}>
-                {currency[1].name} {currency[1].icon}
-              </p>
-            ))}
-          </MobileSelectMenu>
-          <MobileSelectMenu
-            childrenClassName={styles.languages}
-            leftIcon="fas fa-chevron-down"
-            reversableIcon="left"
-            selected={selected.language}
-            format={languageFormatFn}
-            reverseType="180"
-          >
-            {Object.entries(languages).map((language, index) => (
-              <div className={styles.languages}
-                   onMouseDown={() => handleClickOption(language[1], 'language')}
-                   key={`language-${index}`}>
-                <img src={language[1].icon} alt={language[1].name}/>
-                <span>{language[1].name}</span>
+          <div className={styles.navigation}>
+            <div onClick={openSearch} className={styles.menuItem}>
+              <div className={styles.title}>
+                <i className="fas fa-search" />
+                <span>Search</span>
               </div>
-            ))}
-          </MobileSelectMenu>
-        </div>
+              <div className={styles.additionalTitle}>
+                <span>{search!.searchType === 'advanced' ? 'Advanced search' : 'Basic search'}</span>
+                <i className="fas fa-filter" />
+              </div>
+            </div>
+            <MobileSelectMenu
+              title="Explore by country"
+              leftIcon="fas fa-map-marker-alt"
+              rightIcon="fas fa-chevron-right"
+              reversableIcon="right"
+              reverseType="90"
+            >
+              <SelectContinent theme="mobile" handleClose={closeMenu} />
+            </MobileSelectMenu>
+            <div className={styles.menuItem}>
+              <div className={styles.title}>
+                <i className="fas fa-thumbs-up" />
+                <span>Top Deals</span>
+              </div>
+            </div>
+            <div className={styles.menuItem}>
+              <div className={styles.title}>
+                <i className="fas fa-clock" />
+                <span>Last Minute Deals</span>
+              </div>
+            </div>
+            <div className={styles.menuItem}>
+              <div className={styles.title}>
+                <i className="fas fa-user-alt" />
+                <span>Sign up</span>
+              </div>
+            </div>
+            <h5>Settings</h5>
+            <MobileSelectMenu
+              childrenClassName={styles.currencies}
+              leftIcon="fas fa-chevron-down"
+              reversableIcon="left"
+              selected={selected.currency}
+              format={currencyFormatFn}
+              reverseType="180"
+            >
+              {Object.entries(currencies).map((currency, index) => (
+                <p
+                  key={`${currency[0]}-${index}`}
+                  data-currency={currency[1]}
+                  data-field="currency"
+                  onClick={() => handleClickOption(currency[1], 'currency')}
+                >
+                  {currency[1].name} {currency[1].icon}
+                </p>
+              ))}
+            </MobileSelectMenu>
+            <MobileSelectMenu
+              childrenClassName={styles.languages}
+              leftIcon="fas fa-chevron-down"
+              reversableIcon="left"
+              selected={selected.language}
+              format={languageFormatFn}
+              reverseType="180"
+            >
+              {Object.entries(languages).map((language, index) => (
+                <div className={styles.languages} onMouseDown={() => handleClickOption(language[1], 'language')} key={`language-${index}`}>
+                  <img src={language[1].icon} alt={language[1].name} />
+                  <span>{language[1].name}</span>
+                </div>
+              ))}
+            </MobileSelectMenu>
+          </div>
+        </>
       </MobileMenuWrapper>
       <MobileMenuWrapper className={styles.search} open={search!.searchIsOpen}>
-        <div className={styles.searchHeader}>
-          <i onClick={closeSearch} className="fas fa-arrow-left"/>
-          <span onClick={() => changeSearchType('basic')} data-active={search!.searchType === 'basic'}>
-            Basic Search
-          </span>
-          <span onClick={() => changeSearchType('advanced')} data-active={search!.searchType === 'advanced'}>
-            Advanced Search
-          </span>
-        </div>
-        <hr/>
         <>
-          {search!.searchType === 'basic' ? (
-            <>
-              <div className={styles.searchInput}>
-                <input type="text" placeholder="Try Australia" value={searchQuery}
-                       onChange={event => changeSearch(event.target.value)}/>
-                <i className="fas fa-search"/>
-              </div>
-              {countries.filter(country => country.includes(searchQuery)).length > 0 && (
-                <>
-                  <h5>Countries</h5>
-                  <div className={styles.results}>
-                    {countries
-                      .filter(country => country.includes(searchQuery))
-                      .map((country, index) => (
-                        <p key={`${country}-${index}`}>{country}</p>
-                      ))}
-                  </div>
-                </>
-              )}
+          <div className={styles.searchHeader}>
+            <i onClick={closeSearch} className="fas fa-arrow-left" />
+            <span onClick={() => changeSearchType('basic')} data-active={search!.searchType === 'basic'}>
+              Basic Search
+            </span>
+            <span onClick={() => changeSearchType('advanced')} data-active={search!.searchType === 'advanced'}>
+              Advanced Search
+            </span>
+          </div>
+          <hr />
+          <>
+            {search!.searchType === 'basic' ? (
+              <>
+                <div className={styles.searchInput}>
+                  <input type="text" placeholder="Try Australia" value={searchQuery} onChange={event => changeSearch(event.target.value)} />
+                  <i className="fas fa-search" />
+                </div>
+                {countries.filter(country => country.includes(searchQuery)).length > 0 && (
+                  <>
+                    <h5>Countries</h5>
+                    <div className={styles.results}>
+                      {countries
+                        .filter(country => country.includes(searchQuery))
+                        .map((country, index) => (
+                          <p key={`${country}-${index}`}>{country}</p>
+                        ))}
+                    </div>
+                  </>
+                )}
 
-              {cities.filter(city => city.includes(searchQuery)).length > 0 && (
-                <>
-                  <h5>Cities</h5>
-                  <div className={styles.results}>
-                    {cities
-                      .filter(city => city.includes(searchQuery))
-                      .map((city, index) => (
-                        <p key={`${city}-${index}`}>{city}</p>
-                      ))}
-                  </div>
-                </>
-              )}
-            </>
-          ) : (
-            <>
-              <Select bodyTheme="mobile"
-                      selectedOption={selected.startLocation}
-                      options={options}
-                      onChange={value => handleChange(value, 'startLocation')}
-                      theme="dark"/>
-              <div className={styles.radioButtons}>
-                <SearchRadio placeholder="Day Tour" selected={!multiDayTour} onClick={() => selectTourType(false)}/>
-                <SearchRadio placeholder="Multi day Tour" selected={multiDayTour} onClick={() => selectTourType(true)}/>
-              </div>
-              <Select bodyTheme="mobile"
-                      selectedOption={selected.typeOfTour}
-                      options={startLocation}
-                      onChange={value => handleChange(value, 'typeOfTour')}
-                      theme="dark"/>
-              <Select bodyTheme="mobile"
-                      selectedOption={selected.endLocation}
-                      options={options}
-                      onChange={value => handleChange(value, 'endLocation')}
-                      theme="dark"/>
-              <Select bodyTheme="mobile"
-                      selectedOption={selected.numberOfDays}
-                      options={numberOfDays}
-                      onChange={value => handleChange(value, 'numberOfDays')}
-                      theme="dark"/>
-              <div className={styles.advancedSearchButtons}>
-                <Button className={styles.orangeButton} theme="orange" form="rectangled">
-                  SEARCH
-                </Button>
-                <Button className={styles.resetButton} theme="transparent" form='standart' size="sm" onClick={handleClickReset}>Reset search</Button>
-              </div>
-            </>
-          )}
+                {cities.filter(city => city.includes(searchQuery)).length > 0 && (
+                  <>
+                    <h5>Cities</h5>
+                    <div className={styles.results}>
+                      {cities
+                        .filter(city => city.includes(searchQuery))
+                        .map((city, index) => (
+                          <p key={`${city}-${index}`}>{city}</p>
+                        ))}
+                    </div>
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                <Select
+                  bodyTheme="mobile"
+                  selectedOption={selected.startLocation}
+                  options={options}
+                  onChange={value => handleChange(value, 'startLocation')}
+                  theme="dark"
+                />
+                <div className={styles.radioButtons}>
+                  <SearchRadio placeholder="Day Tour" selected={!multiDayTour} onClick={() => selectTourType(false)} />
+                  <SearchRadio placeholder="Multi day Tour" selected={multiDayTour} onClick={() => selectTourType(true)} />
+                </div>
+                <Select
+                  bodyTheme="mobile"
+                  selectedOption={selected.typeOfTour}
+                  options={startLocation}
+                  onChange={value => handleChange(value, 'typeOfTour')}
+                  theme="dark"
+                />
+                <Select
+                  bodyTheme="mobile"
+                  selectedOption={selected.endLocation}
+                  options={options}
+                  onChange={value => handleChange(value, 'endLocation')}
+                  theme="dark"
+                />
+                <Select
+                  bodyTheme="mobile"
+                  selectedOption={selected.numberOfDays}
+                  options={numberOfDays}
+                  onChange={value => handleChange(value, 'numberOfDays')}
+                  theme="dark"
+                />
+                <div className={styles.advancedSearchButtons}>
+                  <Button className={styles.orangeButton} theme="orange" form="rectangled">
+                    SEARCH
+                  </Button>
+                  <Button className={styles.resetButton} theme="transparent" form="standart" size="sm" onClick={handleClickReset}>
+                    Reset search
+                  </Button>
+                </div>
+              </>
+            )}
+          </>
         </>
       </MobileMenuWrapper>
     </div>
