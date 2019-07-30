9a17d36df38ad611d0753ce91a1851a7b98247d3
mufdvr <belora2010@yandex.ru>
2019-07-30 16:56:36 +0300

    reafactor(document): add container for modal

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ export default class CustomDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="modal-root" />
         </body>
       </html>
     )
