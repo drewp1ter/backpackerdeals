596a7df782091d580ebd8ed04e7fa150f024f3af
mufdvr <belora2010@yandex.ru>
2019-08-09 21:47:36 +0300

    feat(BookingCalendarWeek)

---
+++ src/features/deal/components/BookingCalendarWeek/BookingCalendarWeek.tsx
@@ -0,0 +1,15 @@
+import classNames from 'classnames'
+import React from 'react'
+
+import styles from './BookingCalendarWeek.module.scss'
+
+export interface IProps {
+  readonly className?: string
+}
+
+export const BookingCalendarWeek: React.FC<IProps> = ({ className }) => {
+
+  return (
+
+  )
+}
