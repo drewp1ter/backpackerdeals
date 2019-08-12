a2b63b3536868488cd903aae0a0f4315ceeff168
mufdvr <belora2010@yandex.ru>
2019-08-12 18:07:14 +0300

    fix(font-size)

--- src/components/ExposeTime/ExposeTime.tsx
+++ src/components/ExposeTime/ExposeTime.tsx
@@ -14,19 +14,19 @@ export interface IProps {
 export const ExposeTime: React.FC<IProps> = ({ days, hours, minutes, size = 'md', className }) => {
   return (
     <div className={classNames(styles.exposeTime, className)} data-size={size}>
-      <div className={styles.daysBlock}>
+      <div className={styles.days}>
         <p>{days}</p>
-        <p className={styles.timeName}>Days</p>
+        <label>Days</label>
       </div>
       <p className={styles.colon}>:</p>
-      <div className={styles.hoursBlock}>
+      <div className={styles.hours}>
         <p>{hours}</p>
-        <p className={styles.timeName}>Hours</p>
+        <label>Hours</label>
       </div>
       <p className={styles.colon}>:</p>
-      <div className={styles.minutesBlock}>
+      <div className={styles.minutes}>
         <p>{minutes}</p>
-        <p className={styles.timeName}>Minutes</p>
+        <label>Minutes</label>
       </div>
     </div>
   )
