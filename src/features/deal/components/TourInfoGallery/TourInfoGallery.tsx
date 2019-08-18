080b7122cd2eab7527ef32e63286b0b533cd806c
mufdvr <belora2010@yandex.ru>
2019-08-18 17:14:54 +0300

    refactor(standart)

--- src/features/deal/components/TourInfoGallery/TourInfoGallery.tsx
+++ src/features/deal/components/TourInfoGallery/TourInfoGallery.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import { ImageGallery } from '..'
-// import './image-gallery-no-icon.scss'
-import images from './assets'
-
-// import "react-image-gallery/styles/css/image-gallery.css"
+import * as React from 'react'
 
 import classNames from 'classnames'
+import { images } from './assets'
 import styles from './TourInfoGallery.module.scss'
 
 export interface IProps {
@@ -13,9 +9,37 @@ export interface IProps {
 }
 
 export const TourInfoGallery: React.FC<IProps> = ({ className }) => {
+  const renderGalleryControls = () => (
+    <div className={styles.galleryControls}>
+      <img src={images.control} alt="Control Left" />
+      <img src={images.control} alt="Control Right" />
+    </div>
+  )
+
   return (
-    <div className={styles.galleryImages}>
-      <ImageGallery items={images} />
+    <div className={classNames(styles.tourInfoGallery, className)}>
+      <div className={styles.activeTile}>
+        <img src={images.activeTour.src} alt={images.activeTour.alt} />
+        <div className={styles.toSell}>
+          <div className={styles.toSellIcon}>
+            <img src={images.cup} alt="" />
+          </div>
+          <div>
+            Likely
+            <br /> To Sell Out
+          </div>
+        </div>
+
+        {renderGalleryControls()}
+      </div>
+      <div className={styles.tilesContainer}>
+        {images.tours.map((tour, index) => (
+          <div key={tour.alt} className={classNames(styles.tile, !index && styles.active)}>
+            <img src={tour.src} alt={tour.alt} />
+          </div>
+        ))}
+        {renderGalleryControls()}
+      </div>
     </div>
   )
 }
