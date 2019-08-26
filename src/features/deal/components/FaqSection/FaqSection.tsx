b7a363a837a2c426d7878a17c953f4faf35a6c51
mufdvr <belora2010@yandex.ru>
2019-08-26 18:40:55 +0300

    feat(FaqSection): expand cards on mobile view

--- src/features/deal/components/FaqSection/FaqSection.tsx
+++ src/features/deal/components/FaqSection/FaqSection.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React, { useState } from 'react'
+import React from 'react'
 import { FaqCard } from '..'
 import data from './data'
 
@@ -9,23 +9,47 @@ export interface IProps {
   readonly className?: string
 }
 
-export const FaqSection: React.FC<IProps> = ({ className }) => {
-  const [isExpanded, setIsExpanded] = useState<number>(0)
-  const handleClickExpand = (value: number) => setIsExpanded(isExpanded ^ value)
-  const handleToggleExpandAll = () => setIsExpanded(isExpanded ? 0 : -1)
-  const renderCards = () =>
-    data.map((card, idx) => <FaqCard isExpanded={isExpanded} onClickExpand={handleClickExpand} id={idx} key={idx} {...card} />)
+export interface IState {
+  readonly isExpanded: number
+}
 
-  return (
-    <div className={classNames(styles.faqSection, className)}>
-      <div className={styles.head}>
-        <h3>FAQ</h3>
-        <p data-expanded={!!isExpanded} onClick={handleToggleExpandAll}>
-          { isExpanded ? 'Collapse all' : 'Expand all' }
-          <i className="fas fa-angle-double-down" />
-        </p>
+export class FaqSection extends React.Component<IProps, IState> {
+  state = {
+    isExpanded: 0,
+  }
+
+  handleClickExpand = (value: number) => this.setState(prev => ({ isExpanded: prev.isExpanded ^ value }))
+  handleToggleExpandAll = () => this.setState(prev => ({ isExpanded: prev.isExpanded ? 0 : -1 }))
+  renderCards = () => {
+    const { isExpanded } = this.state
+    return data.map((card, idx) => <FaqCard isExpanded={isExpanded} onClickExpand={this.handleClickExpand} id={idx} key={idx} {...card} />)
+  }
+
+  handleResize = () => screen.width < 768 ? this.setState({ isExpanded: -1 }) : this.setState({ isExpanded: 0 })
+
+  componentDidMount = () => {
+    addEventListener('resize', this.handleResize)
+    this.handleResize()
+  }
+
+  componentWillUnmount = () => {
+    removeEventListener('resize', this.handleResize)
+  }
+
+  render = () => {
+    const { className } = this.props
+    const { isExpanded } = this.state
+    return (
+      <div className={classNames(styles.faqSection, className)}>
+        <div className={styles.head}>
+          <h3>FAQ</h3>
+          <p data-expanded={!!isExpanded} onClick={this.handleToggleExpandAll}>
+            {isExpanded ? 'Collapse all' : 'Expand all'}
+            <i className="fas fa-angle-double-down" />
+          </p>
+        </div>
+        <ul>{this.renderCards()}</ul>
       </div>
-      <ul>{renderCards()}</ul>
-    </div>
-  )
+    )
+  }
 }
