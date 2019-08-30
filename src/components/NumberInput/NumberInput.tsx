d408cff0e98ceb024951741134d32ffe55ee458d
mufdvr <belora2010@yandex.ru>
2019-08-30 18:31:25 +0300

    refactor(standart)

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -13,10 +13,11 @@ export const NumberInput: React.FC<IProps> = ({ value, onChange, className }) =>
   const inputRef = useRef<HTMLInputElement>(null)
   const handleIncrement = () => inputRef.current!.stepUp()
   const handleDecrement = () => inputRef.current!.stepDown()
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => onChange && onChange(Number(target.value))
   return (
     <div className={classNames(styles.inputNumber, className)}>
       <button onClick={handleDecrement} />
-      <input ref={inputRef} className={styles.quantity} min="0" name="quantity" value="0" type="number" />
+      <input onChange={handleChange} ref={inputRef} className={styles.quantity} min="0" name="quantity" value={value} type="number" />
       <button className={styles.plus} onClick={handleIncrement} />
     </div>
   )
