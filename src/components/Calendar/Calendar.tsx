1e8cd7f6080fe2bedb2b713c6cedb5aafaec25bc
mufdvr <belora2010@yandex.ru>
2019-09-05 02:46:39 +0300

    feat(Calendar): add months select

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -12,27 +12,67 @@ export interface IProps {
   readonly disablePast?: boolean
 }
 
-export class Calendar extends CalendarBase<IProps, {}> {
-  previousMonth = () => {
-    const { month, year } = this.state
+export interface IState {
+  readonly isMonthsView: boolean
+}
+
+export class Calendar extends CalendarBase<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+      ...this.state,
+      isMonthsView: false,
+    }
+  }
+
+  handleClickPrev = () => {
+    const { month, year, isMonthsView } = this.state
     const { minDate } = this.props
     const current = new Date(year, month, 1).getTime()
     if (minDate && current <= minDate.getTime()) {
       return
     }
-    this.setState({
-      month: month !== 0 ? month - 1 : 11,
-      year: month !== 0 ? year : year - 1,
-    })
+    if (isMonthsView) {
+      this.setState({ year: year - 1 })
+    } else {
+      this.setState({
+        month: month !== 0 ? month - 1 : 11,
+        year: month !== 0 ? year : year - 1,
+      })
+    }
   }
 
-  nextMonth = () => {
-    const { month, year } = this.state
-    this.setState({
-      month: month !== 11 ? month + 1 : 0,
-      year: month !== 11 ? year : year + 1,
-    })
+  handleClickNext = () => {
+    const { month, year, isMonthsView } = this.state
+    if (isMonthsView) {
+      this.setState({ year: year + 1 })
+    } else {
+      this.setState({
+        month: month !== 11 ? month + 1 : 0,
+        year: month !== 11 ? year : year + 1,
+      })
+    }
   }
+
+  handleClickMonth = ({ currentTarget }: React.MouseEvent<HTMLLIElement>) => {
+    const month = Number(currentTarget.dataset.idx)
+    this.setState({ isMonthsView: false, month })
+  }
+
+  handleClickDay = ({ currentTarget }: React.MouseEvent<HTMLLIElement>) => {
+    const dayid = Number(currentTarget.dataset.dayid)
+    const day = this.daysFillEndNulls[dayid]
+    const { onChange, disablePast } = this.props
+    if (disablePast && day && day.getTime() < this.now.getTime()) {
+      return
+    }
+    if (day) {
+      this.setState({ year: day.getFullYear(), month: day.getMonth() })
+      onChange && onChange(day, dayid)
+    }
+  }
+
+  handleToggleMonthsView = () => this.setState(prev => ({ ...prev, isMonthsView: !prev.isMonthsView }))
 
   renderDay = (day: Date | null, idx: number) => {
     const { month, year } = this.state
@@ -50,19 +90,6 @@ export class Calendar extends CalendarBase<IProps, {}> {
     )
   }
 
-  handleClickDay = ({ currentTarget }: React.MouseEvent<HTMLLIElement>) => {
-    const dayid = Number(currentTarget.dataset.dayid)
-    const day = this.daysFillEndNulls[dayid]
-    const { onChange, disablePast } = this.props
-    if (disablePast && day && day.getTime() < this.now.getTime()) {
-      return
-    }
-    if (day) {
-      this.setState({ year: day.getFullYear(), month: day.getMonth() })
-      onChange && onChange(day, dayid)
-    }
-  }
-
   renderHeader = () => {
     return [...Array(7).keys()].map(dayOfWeek => (
       <li key={dayOfWeek}>
@@ -72,20 +99,32 @@ export class Calendar extends CalendarBase<IProps, {}> {
   }
 
   render() {
-    const { month, year } = this.state
+    const { month, year, isMonthsView } = this.state
     const { className } = this.props
 
     return (
       <div className={classNames(styles.calendar, className)}>
         <div className={styles.navigation}>
-          <i className="fas fa-chevron-left" onClick={this.previousMonth} />
-          <div>
-            {this.monthsLong[month]} {year}
+          <i className="fas fa-chevron-left" onClick={this.handleClickPrev} />
+          <div onClick={this.handleToggleMonthsView}>
+            {!isMonthsView && this.monthsLong[month]} {year}
           </div>
-          <i className="fas fa-chevron-right" onClick={this.nextMonth} />
+          <i className="fas fa-chevron-right" onClick={this.handleClickNext} />
         </div>
-        <ul className={styles.header}>{this.renderHeader()}</ul>
-        <ul className={styles.days}>{this.daysFillEndNulls.map(this.renderDay)}</ul>
+        {isMonthsView ? (
+          <ul className={styles.months}>
+            {this.monthsLong.map((month, idx) => (
+              <li onClick={this.handleClickMonth} data-idx={idx} key={idx}>
+                {month}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <>
+            <ul className={styles.header}>{this.renderHeader()}</ul>
+            <ul className={styles.days}>{this.daysFillEndNulls.map(this.renderDay)}</ul>
+          </>
+        )}
       </div>
     )
   }
