7ca982268d46f8d72bf57a171cddf31d06b6bd7b
mufdvr <belora2010@yandex.ru>
2019-09-12 11:34:26 +0300

    refactor(TourGallery): anim duration & image sizes

--- src/features/deal/components/TourGallery/assets/index.ts
+++ src/features/deal/components/TourGallery/assets/index.ts
@@ -17,6 +17,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./1.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./1.jpg?resize&size=272'),
       }
     ],
     original: require('./1.jpg'),
@@ -43,6 +47,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./3.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./3.jpg?resize&size=272'),
       }
     ],
     original: require('./3.jpg'),
@@ -63,6 +71,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./4.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./4.jpg?resize&size=272'),
       }
     ],
     original: require('./4.jpg'),
@@ -83,6 +95,130 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./5.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./5.jpg?resize&size=272'),
+      }
+    ],
+    original: require('./5.jpg'),
+    thumbnail: require('./5.jpg?resize&size=176'),
+    thumbnailAlt: 'thumb',
+    originalAlt: 'image'
+  },
+  {
+    imageSet: [
+      {
+        media: '(max-width: 1920px)',
+        srcSet: require('./1.jpg?resize&size=1136'),
+      },
+      {
+        media: '(max-width: 1024px)',
+        srcSet: require('./1.jpg?resize&size=712'),
+      },
+      {
+        media: '(max-width: 767px)',
+        srcSet: require('./1.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./1.jpg?resize&size=272'),
+      }
+    ],
+    original: require('./1.jpg'),
+    thumbnail: require('./1.jpg?resize&size=176'),
+    thumbnailAlt: 'thumb',
+    originalAlt: 'image'
+  },
+  {
+    imageSet: [
+      {
+        media: '(max-width: 1920px)',
+        srcSet: require('./2.jpg?resize&size=1136'),
+      },
+      {
+        media: '(max-width: 1024px)',
+        srcSet: require('./2.jpg?resize&size=712'),
+      },
+      {
+        media: '(max-width: 767px)',
+        srcSet: require('./2.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./2.jpg?resize&size=272'),
+      }
+    ],
+    original: require('./2.jpg'),
+    thumbnail: require('./2.jpg?resize&size=176'),
+    thumbnailAlt: 'thumb',
+    originalAlt: 'image'
+  },
+  {
+    imageSet: [
+      {
+        media: '(max-width: 1920px)',
+        srcSet: require('./3.jpg?resize&size=1136'),
+      },
+      {
+        media: '(max-width: 1024px)',
+        srcSet: require('./3.jpg?resize&size=712'),
+      },
+      {
+        media: '(max-width: 767px)',
+        srcSet: require('./3.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./3.jpg?resize&size=272'),
+      }
+    ],
+    original: require('./3.jpg'),
+    thumbnail: require('./3.jpg?resize&size=176'),
+    thumbnailAlt: 'thumb',
+    originalAlt: 'image'
+  },
+  {
+    imageSet: [
+      {
+        media: '(max-width: 1920px)',
+        srcSet: require('./4.jpg?resize&size=1136'),
+      },
+      {
+        media: '(max-width: 1024px)',
+        srcSet: require('./4.jpg?resize&size=712'),
+      },
+      {
+        media: '(max-width: 767px)',
+        srcSet: require('./4.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./4.jpg?resize&size=272'),
+      }
+    ],
+    original: require('./4.jpg'),
+    thumbnail: require('./4.jpg?resize&size=176'),
+    thumbnailAlt: 'thumb',
+    originalAlt: 'image'
+  },
+  {
+    imageSet: [
+      {
+        media: '(max-width: 1920px)',
+        srcSet: require('./5.jpg?resize&size=1136'),
+      },
+      {
+        media: '(max-width: 1024px)',
+        srcSet: require('./5.jpg?resize&size=712'),
+      },
+      {
+        media: '(max-width: 767px)',
+        srcSet: require('./5.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./5.jpg?resize&size=272'),
       }
     ],
     original: require('./5.jpg'),
@@ -104,105 +240,10 @@ export const gallery: IItem[] = [
         media: '(max-width: 767px)',
         srcSet: require('./1.jpg?resize&size=652'),
       }
-    ],
-    original: require('./1.jpg'),
-    thumbnail: require('./1.jpg?resize&size=176'),
-    thumbnailAlt: 'thumb',
-    originalAlt: 'image'
-  },
-  {
-    imageSet: [
+      ,
       {
-        media: '(max-width: 1920px)',
-        srcSet: require('./2.jpg?resize&size=1136'),
-      },
-      {
-        media: '(max-width: 1024px)',
-        srcSet: require('./2.jpg?resize&size=712'),
-      },
-      {
-        media: '(max-width: 767px)',
-        srcSet: require('./2.jpg?resize&size=652'),
-      }
-    ],
-    original: require('./2.jpg'),
-    thumbnail: require('./2.jpg?resize&size=176'),
-    thumbnailAlt: 'thumb',
-    originalAlt: 'image'
-  },
-  {
-    imageSet: [
-      {
-        media: '(max-width: 1920px)',
-        srcSet: require('./3.jpg?resize&size=1136'),
-      },
-      {
-        media: '(max-width: 1024px)',
-        srcSet: require('./3.jpg?resize&size=712'),
-      },
-      {
-        media: '(max-width: 767px)',
-        srcSet: require('./3.jpg?resize&size=652'),
-      }
-    ],
-    original: require('./3.jpg'),
-    thumbnail: require('./3.jpg?resize&size=176'),
-    thumbnailAlt: 'thumb',
-    originalAlt: 'image'
-  },
-  {
-    imageSet: [
-      {
-        media: '(max-width: 1920px)',
-        srcSet: require('./4.jpg?resize&size=1136'),
-      },
-      {
-        media: '(max-width: 1024px)',
-        srcSet: require('./4.jpg?resize&size=712'),
-      },
-      {
-        media: '(max-width: 767px)',
-        srcSet: require('./4.jpg?resize&size=652'),
-      }
-    ],
-    original: require('./4.jpg'),
-    thumbnail: require('./4.jpg?resize&size=176'),
-    thumbnailAlt: 'thumb',
-    originalAlt: 'image'
-  },
-  {
-    imageSet: [
-      {
-        media: '(max-width: 1920px)',
-        srcSet: require('./5.jpg?resize&size=1136'),
-      },
-      {
-        media: '(max-width: 1024px)',
-        srcSet: require('./5.jpg?resize&size=712'),
-      },
-      {
-        media: '(max-width: 767px)',
-        srcSet: require('./5.jpg?resize&size=652'),
-      }
-    ],
-    original: require('./5.jpg'),
-    thumbnail: require('./5.jpg?resize&size=176'),
-    thumbnailAlt: 'thumb',
-    originalAlt: 'image'
-  },
-  {
-    imageSet: [
-      {
-        media: '(max-width: 1920px)',
-        srcSet: require('./1.jpg?resize&size=1136'),
-      },
-      {
-        media: '(max-width: 1024px)',
-        srcSet: require('./1.jpg?resize&size=712'),
-      },
-      {
-        media: '(max-width: 767px)',
-        srcSet: require('./1.jpg?resize&size=652'),
+        media: '(max-width: 320px)',
+        srcSet: require('./1.jpg?resize&size=272'),
       }
     ],
     original: require('./1.jpg'),
@@ -223,6 +264,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./2.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./2.jpg?resize&size=272'),
       }
     ],
     original: require('./2.jpg'),
@@ -243,6 +288,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./3.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./3.jpg?resize&size=272'),
       }
     ],
     original: require('./3.jpg'),
@@ -263,6 +312,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./4.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./4.jpg?resize&size=272'),
       }
     ],
     original: require('./4.jpg'),
@@ -283,6 +336,10 @@ export const gallery: IItem[] = [
       {
         media: '(max-width: 767px)',
         srcSet: require('./5.jpg?resize&size=652'),
+      },
+      {
+        media: '(max-width: 320px)',
+        srcSet: require('./5.jpg?resize&size=272'),
       }
     ],
     original: require('./5.jpg'),
