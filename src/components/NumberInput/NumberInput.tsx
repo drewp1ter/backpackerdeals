0a3acf6d4e3b50d1ae7259d7c0ff58dd4d73aa48
mufdvr <belora2010@yandex.ru>
2019-09-16 11:14:27 +0300

    feat(NumberInput): add min max values

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -4,20 +4,32 @@ import React, { useRef } from 'react'
 import styles from './NumberInput.module.scss'
 
 export interface IProps {
+  readonly min?: number
+  readonly max?: number
   readonly value?: number
-  readonly onChange?: (value: number) => void
+  readonly name?: string
+  readonly onChange?: (value: number, name?: string) => void
   readonly className?: string
 }
 
-export const NumberInput: React.FC<IProps> = ({ value, onChange, className }) => {
+export const NumberInput: React.FC<IProps> = ({ value, onChange, className, min, max, name }) => {
   const inputRef = useRef<HTMLInputElement>(null)
   const handleIncrement = () => inputRef.current!.stepUp()
   const handleDecrement = () => inputRef.current!.stepDown()
-  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => onChange && onChange(Number(target.value))
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => onChange && onChange(Number(target.value), name || '')
   return (
     <div className={classNames(styles.inputNumber, className)}>
       <button onClick={handleDecrement} />
-      <input onChange={handleChange} ref={inputRef} className={styles.quantity} min="0" name="quantity" value={value} type="number" />
+      <input
+        onChange={handleChange}
+        ref={inputRef}
+        className={styles.quantity}
+        min={min}
+        max={max}
+        name="quantity"
+        value={value}
+        type="number"
+      />
       <button className={styles.plus} onClick={handleIncrement} />
     </div>
   )
