8568649042e53f3a9d42e3f9c932224b5765cabd
mufdvr <belora2010@yandex.ru>
2019-09-16 17:26:03 +0300

    feat(Helmet): ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import withRedux from 'next-redux-wrapper'
 import App, { Container } from 'next/app'
 import * as React from 'react'
+import Helmet from 'react-helmet'
 import { Provider, ProviderProps } from 'react-redux'
 import makeStore from 'store'
 
-// import '@fortawesome/fontawesome-free/css/all.css'
 import 'fontAwesome.css'
 import 'normalize.css'
 import 'scss/root.scss'
@@ -15,7 +15,20 @@ class MyApp extends App<ProviderProps> {
     return (
       <Container>
         <Provider store={store}>
-          <Component {...pageProps} />
+          <>
+            <Helmet
+              htmlAttributes={{ lang: 'en' }}
+              title="Backpackerdeals"
+              meta={[
+                {
+                  name: 'viewport',
+                  content: 'width=device-width, initial-scale=1',
+                },
+                { property: 'og:title', content: 'Backpackerdeals' },
+              ]}
+            />
+            <Component {...pageProps} />
+          </>
         </Provider>
       </Container>
     )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,40 @@
 import { InlineStylesHead } from 'components/InlineStylesHead'
 import Document, { Head, Main, NextScript } from 'next/document'
+import Helmet, { HelmetData } from 'react-helmet'
 
-export default class CustomDocument extends Document {
-  static getInitialProps({ renderPage }: any) {
+export interface IProps {
+  readonly helmet: HelmetData
+}
+
+export default class CustomDocument extends Document<IProps> {
+  static async getInitialProps({ renderPage }: any) {
     const { html, head, errorHtml, chunks } = renderPage()
-    return { html, head, errorHtml, chunks }
+    return { html, head, errorHtml, chunks, helmet: Helmet.renderStatic() }
+  }
+
+  // should render on <html>
+  get helmetHtmlAttrComponents () {
+    return this.props.helmet.htmlAttributes.toComponent()
+  }
+
+  // should render on <body>
+  get helmetBodyAttrComponents () {
+    return  this.props.helmet.bodyAttributes.toComponent()
+  }
+
+  // should render on <head>
+  get helmetHeadComponents () {
+    return Object.keys(this.props.helmet)
+      .filter(el => el !== 'htmlAttributes' && el !== 'bodyAttributes')
+      .map(el => this.props.helmet[el as keyof HelmetData].toComponent())
   }
 
   render() {
     const HeadVar = process.env.NODE_ENV === 'production' ? InlineStylesHead : Head
     return (
-      <html lang="en">
-        <HeadVar />
-        <body>
+      <html {...this.helmetHtmlAttrComponents}>
+        <HeadVar>{this.helmetHeadComponents}</HeadVar>
+        <body {...this.helmetBodyAttrComponents}>
           <Main />
           <NextScript />
           <div id="modal-root" />
