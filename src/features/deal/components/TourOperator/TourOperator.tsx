955bccc5940f39fa1ee24918c1b70945ffe28a8c
mufdvr <belora2010@yandex.ru>
2019-09-17 19:59:05 +0300

    refactor(standart)

--- src/features/deal/components/TourOperator/TourOperator.tsx
+++ src/features/deal/components/TourOperator/TourOperator.tsx
@@ -14,7 +14,7 @@ export const TourOperator: React.FC<IProps> = ({ className }) => {
       <h3>Tour Operator</h3>
       <div className={styles.content}>
         <div className={styles.iframeWrapper}>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9050548.165515205!2d129.49759360932202!3d-26.17454032372351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2b32194a62bd9563%3A0xe3e1402ed4be27f2!2sWayoutback+Australian+Safaris!5e0!3m2!1sru!2sru!4v1565043312088!5m2!1sru!2sru" />
+          {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9050548.165515205!2d129.49759360932202!3d-26.17454032372351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2b32194a62bd9563%3A0xe3e1402ed4be27f2!2sWayoutback+Australian+Safaris!5e0!3m2!1sru!2sru!4v1565043312088!5m2!1sru!2sru" /> */}
         </div>
         <div className={styles.card}>
           <h4>Tour Operator</h4>
