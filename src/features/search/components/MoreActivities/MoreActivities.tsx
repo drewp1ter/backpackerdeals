7a381bb4ec3da6017d2a56badff91774a0784655
mufdvr <belora2010@yandex.ru>
2019-10-17 13:19:15 +0300

    refactor(MoreActivities): grid

--- src/features/search/components/MoreActivities/MoreActivities.tsx
+++ src/features/search/components/MoreActivities/MoreActivities.tsx
@@ -51,7 +51,7 @@ export const MoreActivities: React.FC<IProps> = ({ className }) => {
       <FiltersMenu />
       <div className={styles.content}>
         <Filters className={styles.filters} filters={filters} />
-        <div className={styles.cardsContainer}>
+        <div>
           <div className={styles.header}>
             <span>{cards.length} Activities</span>
             <span className={styles.viewStyle}>
